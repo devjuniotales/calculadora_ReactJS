87ecf24ab8372f6bde6369aa248c0496ca2dbb77
devjuniotales <junio.thales@hotmail.com>
2021-04-28 00:15:00 -0300

    update

--- src/App.js
+++ src/App.js
@@ -73,26 +73,39 @@ function App() {
            {value}
         </div>
        <div className ='content-button'>
-          <button className ='button'value ='C' type ='button' onClick ={() => clear() }>C</button>
-          <button className ='button'value ='DEL' type ='button' onClick ={() => {}}>DEL</button>
+       <div className ='buttonarea'>
+          <button className ='buttonc'value ='C' type ='button' onClick ={() => clear() }>C</button>
           <button className ='button'value ='%' type ='button' onClick ={(event) => operator(event.target.value)}>%</button>
           <button className ='button'value ='/' type ='button' onClick ={(event) => operator(event.target.value)}>/</button>
+       </div>
+
+         <div className ='buttonarea'>
           <button className ='button'value ='9' type ='button' onClick ={event => addNumber(event.target.value)}>9</button>
           <button className ='button'value ='8' type ='button' onClick ={event => addNumber(event.target.value)}>8</button>
           <button className ='button'value ='7' type ='button' onClick ={event => addNumber(event.target.value)}>7</button>
-          <button className ='button'value ='*' type ='button' onClick ={(event) => operator(event.target.value)}>*</button>
-          <button className ='button'value ='4' type ='button' onClick ={event => addNumber(event.target.value)}>4</button>
-          <button className ='button'value ='5' type ='button' onClick ={event => addNumber(event.target.value)}>5</button>
-          <button className ='button'value ='6' type ='button' onClick ={event => addNumber(event.target.value)}>6</button>
-          <button className ='button'value ='-' type ='button' onClick ={(event) => operator(event.target.value)}>-</button>
-          <button className ='button'value ='1' type ='button' onClick ={event => addNumber(event.target.value)}>1</button>
-          <button className ='button'value ='2' type ='button' onClick ={event => addNumber(event.target.value)}>2</button>
-          <button className ='button'value ='3' type ='button' onClick ={event => addNumber(event.target.value)}>3</button>
-          <button className ='button'value ='+' type ='button' onClick ={(event) => operator(event.target.value)}>+</button>
-          <button className ='button'value =' ' type ='button' onClick ={(event) => operator(event.target.value)}> </button>
-          <button className ='button'value ='0' type ='button' onClick ={event => addNumber(event.target.value)}>0</button>
-          <button className ='button'value =',' type ='button' onClick ={event => virgula(event.target.value)}>,</button>
-          <button className ='button'value ='=' type ='button' onClick ={() => calculate()}>=</button>
+            <button className ='button'value ='+' type ='button' onClick ={(event) => operator(event.target.value)}>+</button>
+         </div>
+
+         <div className ='buttonarea'>
+            <button className ='button'value ='4' type ='button' onClick ={event => addNumber(event.target.value)}>4</button>
+            <button className ='button'value ='5' type ='button' onClick ={event => addNumber(event.target.value)}>5</button>
+            <button className ='button'value ='6' type ='button' onClick ={event => addNumber(event.target.value)}>6</button>
+            <button className ='button'value ='*' type ='button' onClick ={(event) => operator(event.target.value)}>*</button>
+         </div>
+
+         <div className ='buttonarea'>
+            <button className ='button'value ='1' type ='button' onClick ={event => addNumber(event.target.value)}>1</button>
+            <button className ='button'value ='2' type ='button' onClick ={event => addNumber(event.target.value)}>2</button>
+            <button className ='button'value ='3' type ='button' onClick ={event => addNumber(event.target.value)}>3</button>
+            <button className ='button'value ='-' type ='button' onClick ={(event) => operator(event.target.value)}>-</button>
+            
+         </div>
+
+          <div className ='buttonarea'>
+            <button className ='buttonc'value ='0' type ='button' onClick ={event => addNumber(event.target.value)}>0</button>
+            <button className ='button'value =',' type ='button' onClick ={event => virgula(event.target.value)}>,</button>
+            <button className ='button'value ='=' type ='button' onClick ={() => calculate()}>=</button>
+          </div>
        </div>
       </div>
     </div>
